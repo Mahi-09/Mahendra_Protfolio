a12f1a031d39b805b8c5e828a431dbb214e8eefa
Mahi_09 <darlingmahee09@gmail.com>
2024-08-11 00:40:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='whole'>
+          <div class="hero">
+            <nav>
+                <img src="Untitled-removebg.png" class="logo"></img>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="https://drive.google.com/drive/folders/1HqA4PDP9BxZKcxhPfADz4lFTOaaPc0e1?usp=drive_link">ACADAMICS</a></li>
+                    <li><a href="">PROJECTS</a></li>
+                    <li><a href="https://drive.google.com/file/d/1jkH-tKeRL8qcK-d4GHAB0ed80vrCv21q/view?usp=drivesdk">CERTIFICATION</a></li>
+                    <li><a href="#">HIRE ME</a></li>
+                </ul>
+            </nav>
+          </div>
+          <div classe="links">
+            <a href="https://www.instagram.com/mr.mahi_0009?utm_source=qr&igsh=MW05OGVwYWVwb2x2bA=="><img src="instagram.png" class="insta"></img></a>
+            <a href="https://www.linkedin.com/in/patnam-mahendra-553462259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src="linkedin.png" class="lin"></img></a>
+            <a href="https://wa.me/qr/4ZKUQGUP7GPDC1 "><img src="whatsapp.png" class="what"></img></a>
+          </div>
+          <div class="details">
+            <h1> I am Patnam <span> Mahendra</span></h1>
+            <p> This is my Official Protfolio website 
+                <br></br> and Work Experience.
+            <br></br> <h3>@Front-end Developer</h3>
+            </p>
+            <a href="https://drive.google.com/file/d/1jPWaDE5alSkpFdNLwsn-Ej8S956GrEKq/view?usp=drive_link">DOWNLOAD CV</a>
+        </div>
+        <div class="images">
+            <img src="blob-design-shape-with-transparent-background-png.webp" class="shape"></img>
+            <img src="1000083420-removebg-preview.png" class="boy"></img>
+
+        </div>
     </div>
   );
 }
-
 export default App;
