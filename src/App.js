3872cc601bc844b99740fd69f84b369c38349810
Mahi_09 <darlingmahee09@gmail.com>
2024-08-14 00:36:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,17 +8,42 @@ function App() {
           <div class="hero">
             <nav>
                 <img src="Untitled-removebg.png" class="logo"></img>
+                
                 <ul>
-                    <li><a href="#">HOME</a></li>
-                    <li><a href="https://drive.google.com/drive/folders/1HqA4PDP9BxZKcxhPfADz4lFTOaaPc0e1?usp=drive_link">ACADAMICS</a></li>
-                    <li><a href="">PROJECTS</a></li>
-                    <li><a href="https://drive.google.com/file/d/1jkH-tKeRL8qcK-d4GHAB0ed80vrCv21q/view?usp=drivesdk">CERTIFICATION</a></li>
-                    <li><a href="#">HIRE ME</a></li>
+                    <li class="dropdown">
+                      <a href="javascript:void(0)" class="dropbtn">HOME</a>
+                      <div class="dropdown-content">
+                        <a href="#">WELCOME..!</a>
+                      </div>
+                    </li>
+                    <li class="dropdown">
+                      <a href="javascript:void(0)" class="dropbtn">ACADAMICS</a>
+                      <div class="dropdown-content">
+                        <a href="https://drive.google.com/file/d/1ZCYYz8JV2qmswGCivK7TTzt6kbYIxTGv/view?usp=drive_link">B.TECH</a>
+                        <a href="https://drive.google.com/file/d/12kEwUg9A9XBWob1Z_uSVrm8-7YXHuBAV/view?usp=drive_link">INTERMEDIATE</a>
+                        <a href="https://drive.google.com/file/d/1zYH177R8BhZJojB_ImTR1Ey7XolTFYNk/view?usp=drive_link">TENTH</a>
+                      </div>
+                    </li>
+                    <li class="dropdown">
+                      <a href="javascript:void(0)" class="dropbtn">CERTIFICATES</a>
+                      <div class="dropdown-content">
+                        <a href="https://drive.google.com/file/d/1lujmBWGVuRLj5TUsaEE1WKVQvpGU-3yU/view?usp=drive_link">GOOGLE</a>
+                        <a href="https://drive.google.com/file/d/1lcJVfK1F6X7hHvepQSjcocCVzeGoKc9y/view?usp=drive_link">INTERNSHIP(IIDT)</a>
+                        <a href="https://drive.google.com/file/d/1HFbVwZUrWWf2UZMRdHc-xp_lXcfDGNIY/view?usp=drive_link">BRAINOVISION</a>
+                      </div>
+                    </li>
+                    <li class="dropdown">
+                      <a href="javascript:void(0)" class="dropbtn">CONTACT</a>
+                      <div class="dropdown-content">
+                        <a href="6309628854">CALL ME</a>
+                      </div>
+                    </li>
                 </ul>
+                
             </nav>
           </div>
           <div classe="links">
-            <a href="https://www.instagram.com/mr.mahi_0009?utm_source=qr&igsh=MW05OGVwYWVwb2x2bA=="><img src="instagram.png" class="insta"></img></a>
+            <a href="https://www.instagram.com/mr.mahi_0009?utm_source=qr&igsh=MW05OGVwYWVwb2x2bA== "><img src="instagram.png" class="insta"></img></a>
             <a href="https://www.linkedin.com/in/patnam-mahendra-553462259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src="linkedin.png" class="lin"></img></a>
             <a href="https://wa.me/qr/4ZKUQGUP7GPDC1 "><img src="whatsapp.png" class="what"></img></a>
           </div>
@@ -28,11 +53,11 @@ function App() {
                 <br></br> and Work Experience.
             <br></br> <h3>@Front-end Developer</h3>
             </p>
-            <a href="https://drive.google.com/file/d/1jPWaDE5alSkpFdNLwsn-Ej8S956GrEKq/view?usp=drive_link">DOWNLOAD CV</a>
+            <a href="https://drive.google.com/file/d/1XtirS1SF2c8XGGrv5EAOatTcI0erLFoZ/view?usp=sharing">DOWNLOAD CV</a>
         </div>
         <div class="images">
-            <img src="blob-design-shape-with-transparent-background-png.webp" class="shape"></img>
-            <img src="1000083420-removebg-preview.png" class="boy"></img>
+            {/* <img src="blob-design-shape-with-transparent-background-png.webp" class="shape"></img> */}
+            <img src="Picsart_24-08-12_21-51-03-515.png" class="boy"></img>
 
         </div>
     </div>
